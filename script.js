c0bf258f8b939abbf0dec2d23152bfe8b521be7c
William <williamhess@gmail.com>
2023-09-23 22:12:38 -0700

    Add validation and style to form

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log('Hi');
+let password = document.querySelector("#password").value;
+let confirmPassword = document.querySelector("#confirmPassword").value;
+
+if (password != confirmPassword) {
+    alert("Your passwords don't match.");
+}
+
+document.getElementById("#myForm").onsubmit(function(e) {
+    e.preventDefault();
+ });
