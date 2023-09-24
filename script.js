13959d09ed1d8d85f351322794f47613eb5d541c
William <williamhess@gmail.com>
2023-09-24 16:23:42 -0700

    Add style

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
+// let password = document.querySelector("#password").value;
+// let confirmPassword = document.querySelector("#confirmPassword").value;
+
+// if (password != confirmPassword) {
+//     alert("Your passwords don't match.");
+// }
+
+// document.getElementById("#myForm").onsubmit(function(e) {
+//     e.preventDefault();
+//  });
+
+console.log('hi');
+
 let password = document.querySelector("#password").value;
 let confirmPassword = document.querySelector("#confirmPassword").value;
 
 if (password != confirmPassword) {
-    alert("Your passwords don't match.");
+  alert("Your passwords don't match.");
 }
 
-document.getElementById("#myForm").onsubmit(function(e) {
-    e.preventDefault();
- });
+document.getElementById("myForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+});
