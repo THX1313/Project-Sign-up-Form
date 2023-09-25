74e902ff6bb51695a9ce7b91f62257365a47db24
William <williamhess@gmail.com>
2023-09-24 21:58:24 -0700

    Fix code comparing password inputs

--- script.js
+++ script.js
@@ -1,23 +1,10 @@
-// let password = document.querySelector("#password").value;
-// let confirmPassword = document.querySelector("#confirmPassword").value;
-
-// if (password != confirmPassword) {
-//     alert("Your passwords don't match.");
-// }
-
-// document.getElementById("#myForm").onsubmit(function(e) {
-//     e.preventDefault();
-//  });
-
-console.log('hi');
-
-let password = document.querySelector("#password").value;
-let confirmPassword = document.querySelector("#confirmPassword").value;
-
-if (password != confirmPassword) {
-  alert("Your passwords don't match.");
-}
-
 document.getElementById("myForm").addEventListener("submit", function(e) {
   e.preventDefault();
+
+  let password = document.querySelector("#password").value.trim();
+  let confirmPassword = document.querySelector("#confirmPassword").value.trim();
+
+  if (password !== confirmPassword) {
+    alert("Your passwords don't match.");
+  }
 });
